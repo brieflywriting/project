35032646749c39f372cdb212c2c0268440bc195b
NotTheSharpestToolInAShed <44844444+NotTheSharpestToolInAShed@users.noreply.github.com>
2020-08-14 20:59:46 +0300

    Main
    First commit

---
+++ tiled wms/index.js
@@ -0,0 +1,124 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import TileWMS from 'ol/source/TileWMS';
+import 'ol/ol.css';
+import GeoJSON from 'ol/format/GeoJSON';
+import LinearRing from 'ol/geom/LinearRing';
+import VectorSource from 'ol/source/Vector';
+import {
+  LineString,
+  MultiLineString,
+  MultiPoint,
+  MultiPolygon,
+  Point,
+  Polygon,
+} from 'ol/geom';
+import { Vector as VectorLayer} from 'ol/layer';
+import {fromLonLat} from 'ol/proj';
+
+
+
+
+
+//#region jsts buffer import 
+var source2 = new VectorSource();
+fetch('./data/geojson/citizens.geojson')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    var format = new GeoJSON();
+    var features = format.readFeatures(json, {
+      featureProjection: 'EPSG:4326',
+    });
+
+    var parser = new jsts.io.OL3Parser();
+    parser.inject(
+      Point,
+      LineString,
+      LinearRing,
+      Polygon,
+      MultiPoint,
+      MultiLineString,
+      MultiPolygon
+    );
+
+    for (var i = 0; i < features.length; i++) {
+      var feature = features[i];
+      // convert the OpenLayers geometry to a JSTS geometry
+      var jstsGeom = parser.read(feature.getGeometry());
+
+      // create a buffer of 40 meters around each line
+      var buffered = jstsGeom.buffer(40);
+
+      // convert back from JSTS and replace the geometry on the feature
+      feature.setGeometry(parser.write(buffered));
+    }
+
+    source2.addFeatures(features);
+  });
+  //#endregion
+//#region  Исполнение в консоли забития в postgis shp
+  const shell = require('node-powershell');
+  let ps = new shell({
+    executionPolicy: 'Bypass',
+    noProfile: true
+  });
+  
+  ps.addCommand('shp2pgsql -s <SRID> -c -D -I <path to shapefile> <schema>.<table> | \ ');
+  ps.invoke().then(output => {
+    console.log(output);
+  }).catch(err => {
+    console.log(err);
+    ps.dispose();
+  });
+  //#endregion
+var layers = [
+  new TileLayer({
+    source: new OSM()
+  }),
+  new VectorLayer({
+    source: source2,
+  }),
+  new TileLayer({
+   
+    source: new TileWMS({
+      url: 'http://localhost:8080/geoserver/moscow/wms',
+      params: {'LAYERS': 'moscow:border', 'TILED': true, 'TRANSPARENT':true},
+      serverType: 'geoserver',
+      // Countries have transparency, so do not fade tiles:
+      transition: 0
+    })
+  }), new TileLayer({
+   
+    source: new TileWMS({
+      url: 'http://localhost:8080/geoserver/moscow/wms',
+      params: {'LAYERS': 'moscow:citizens', 'TILED': true},
+      serverType: 'geoserver',
+      // Countries have transparency, so do not fade tiles:
+      transition: 0
+    })
+  }), new TileLayer({
+   
+    source: new TileWMS({
+      url: 'http://localhost:8080/geoserver/moscow/wms',
+      params: {'LAYERS': 'moscow:poi', 'TILED': true},
+      serverType: 'geoserver',
+      // Countries have transparency, so do not fade tiles:
+      transition: 0
+    })
+  })
+];
+
+var map = new Map({
+  layers: layers,
+  target: 'map',
+  view: new View({
+    center:fromLonLat( [37.520193,55.613960]),
+    zoom: 13
+    
+  })
+});
